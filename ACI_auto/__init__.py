2ec71bcff6382ecbd6a2e98ae0b9615e7169753c
Rohan Sangodkar <85158624+rohansangodkar@users.noreply.github.com>
2024-07-26 14:55:45 +0530

    Update __init__.py

--- ACI_auto/__init__.py
+++ ACI_auto/__init__.py
@@ -22,7 +22,7 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'rohan.sangodkar@aciworldwide.com'
+app.config['MAIL_USERNAME'] = 'xxx@aciworldwide.com'
 app.config['MAIL_PASSWORD'] = 'Welcome@000'
 mail = Mail(app)
 
